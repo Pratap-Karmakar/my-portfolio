aad6180466946d2e0e8936bfeddca7c258acfc54
Pratap Karmakar <pratap2501.pk@gmail.com>
2024-12-08 19:45:54 +0530

    added projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,7 @@ import sharkchat from "../public/projects/sharkchat.png";
 import foxmovies from '../public/projects/foxmovies.png'
 import jewellery from '../public/projects/jewellery.png'
 import codecraftstudio from '../public/projects/codecraftstudio.png'
+import hershield from '../public/projects/hershield.png'
 import { TbBrandGithub } from "react-icons/tb";
 import { BsGlobe2 } from "react-icons/bs";
 
@@ -203,7 +204,7 @@ const Projects = () => {
               </p>
               <h3 className="text-2xl font-bold">CodeCraft Studio</h3>
               <p className="bg-[#333232] xl:-mr-16 shadow-navbarShadow text-sm md:text-base p-2 md:p-6 rounded-md">
-                A RealTime Chat web site built with{" "}
+                A company web site built with{" "}
                 <span className="text-textOrange">
                   Next.js, tailwind, TypeScript, Framer Motiion
                 </span>{" "}
@@ -285,6 +286,61 @@ const Projects = () => {
                 <a
                   className="text-textOrange"
                   href="https://octoworld.vercel.app/"
+                  target="_blank"
+                >
+                  <BsGlobe2 className="animate-pulse hover:animate-none"/>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 6 */}
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row-reverse gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              href="https://hershield.vercel.app/"
+              target="_blank"
+            >
+              <div>
+                <Image
+                  src={hershield}
+                  alt="project image"
+                  className="w-full h-full object-contain"
+                />
+              </div>
+            </a>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right z-10">
+              <p className="font-titleFont text-textOrange text-sm tracking-wide">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold">HerShield</h3>
+              <p className="bg-[#333232] xl:-mr-16 shadow-navbarShadow text-sm md:text-base p-2 md:p-6 rounded-md">
+                A women safety web site built with{" "}
+                <span className="text-textOrange">
+                  Next.js, tailwind, JavaScript, Framer Motiion
+                </span>{" "}
+                The HerShield website is a women&#39;s safety platform that provides features like an SOS alert system, GPS location sharing, a siren for deterrence, and voice recording for evidence. It also offers intelligent route planning, helpline integration, and a community platform for sharing tips and incidents, ensuring privacy and accessibility through advanced technologies.
+              </p>
+              <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+                <li>Next.js</li>
+                <li>Tailwind</li>
+                <li>JavaScript</li>
+                <li>Framer Motiion</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+                <a
+                  className="text-textOrange"
+                  href="https://github.com/Pratap-Karmakar/newwomensafety"
+                  target="_blank"
+                >
+                  <TbBrandGithub className="animate-pulse hover:animate-none"/>
+                </a>
+
+                <a
+                  className="text-textOrange"
+                  href="https://hershield.vercel.app/"
                   target="_blank"
                 >
                   <BsGlobe2 className="animate-pulse hover:animate-none"/>
